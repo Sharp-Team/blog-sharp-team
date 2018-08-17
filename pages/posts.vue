bdb03f3be47087e9b81ba5e63134b99b29c41ff4
thaycacac <thaycacac@gmail.com>
2018-08-18 00:08:09 +0700

    upload siderbar

--- pages/posts.vue
+++ pages/posts.vue
@@ -6,23 +6,20 @@
         <div v-for="post in posts" :key="post.ID">
           <!-- <nuxt-link
           :to="'/post/' + post.ID"> -->
-            <post-preview
-            :id="post.ID"
-            :title="post.title"
-            :urlImage="post.featured_image"
-            :urlAvata="post.author.avatar_URL"
-            :firstName="post.author.first_name"
-            :lastName="post.author.last_name"
-            :user="post.author.login"
-            :excerpt="post.excerpt"
-            :time="post.modified">
-            </post-preview>
+          <post-preview :id="post.ID" :title="post.title" :urlImage="post.featured_image" :urlAvata="post.author.avatar_URL" :firstName="post.author.first_name"
+            :lastName="post.author.last_name" :user="post.author.login" :excerpt="post.excerpt" :time="post.modified">
+          </post-preview>
           <!-- </nuxt-link> -->
           <br>
         </div>
       </div>
       <div class="column">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis ipsam dolore quam id tenetur sequi quasi consequatur, assumenda rerum at modi. Id sit possimus rem omnis in inventore iste a.
+        <hoapn></hoapn>
+        <ducpv></ducpv>
+        <nhattq></nhattq>
+        <cuongnx></cuongnx>
+        <manhnt></manhnt>
+        <hiepdq></hiepdq>
       </div>
     </div>
   </div>
@@ -30,13 +27,25 @@
 
 <script>
   import PostPreview from '../components/Blog/PostPreview'
-  import ImageTeam from '../components/Header/ImageTeam.vue';
+  import ImageTeam from '../components/Header/ImageTeam.vue'
+  import hoapn from '../components/SliderBar/hoapn.vue'
+  import ducpv from '../components/SliderBar/ducpv.vue'
+  import nhattq from '../components/SliderBar/nhattq.vue'
+  import cuongnx from '../components/SliderBar/cuongnx.vue'
+  import manhnt from '../components/SliderBar/manhnt.vue'
+  import hiepdq from '../components/SliderBar/hiepdq.vue'
   import api from '../api/index.js'
   import config from '../api/config'
   export default {
     components: {
       PostPreview,
-      ImageTeam
+      ImageTeam,
+      hoapn,
+      ducpv,
+      nhattq,
+      cuongnx,
+      manhnt,
+      hiepdq
     },
     // async asyncData({ app }) {
     //   const ip = await app.$axios.$get(config.baseUrl + 'posts')
@@ -59,6 +68,9 @@
       this.display()
     },
   }
+
 </script>
 <style>
+
+
 </style>
